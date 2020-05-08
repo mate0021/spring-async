da2a6a3017c9e580b82345f9e298df7800faef6a
mate00 <mami.ext@gmail.com>
2020-05-08 09:31:36 +0200

    Async call plus handling of compressed response.

--- src/main/java/rnd/mate00/springasync/ApplicationConfiguration.java
+++ src/main/java/rnd/mate00/springasync/ApplicationConfiguration.java
@@ -3,13 +3,33 @@ package rnd.mate00.springasync;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 public class ApplicationConfiguration {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        // need to specify accepted encoding as StackOverflow API always sends compressed response:
+        // https://api.stackexchange.com/docs/compression
+        RestTemplate template = builder.build();
+        template.getInterceptors().add((httpRequest, bytes, clientHttpRequestExecution) -> {
+            httpRequest.getHeaders().set(HttpHeaders.ACCEPT_ENCODING, "gzip");
+            return clientHttpRequestExecution.execute(httpRequest, bytes);
+        });
+
+        return template;
+    }
+
+    @Bean
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setThreadNamePrefix("SO-Lookup-");
+
+        return executor;
     }
 }
--- src/main/java/rnd/mate00/springasync/SpringAsyncApplication.java
+++ src/main/java/rnd/mate00/springasync/SpringAsyncApplication.java
@@ -1,13 +1,30 @@
 package rnd.mate00.springasync;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.CompletableFuture;
+
 @SpringBootApplication
-public class SpringAsyncApplication {
+public class SpringAsyncApplication implements CommandLineRunner {
+
+    @Autowired
+    private StackOverflowLookupService stackOverflowLookupService;
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAsyncApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        CompletableFuture<StackOverflowUser> user1 = stackOverflowLookupService.findUserBy("1534211");
+
+        CompletableFuture.allOf(user1).join();
+
+        System.out.println(user1.get());
     }
 }
